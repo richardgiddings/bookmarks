2f60e2080a02ab0cd1b988bdd5977e1ffe428ac1
Richard Giddings <mail@richardgiddings.co.uk>
2017-04-25 16:08:48 +0100

    Moved home page to bookmark app

--- django_login/urls.py
+++ django_login/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
@@ -33,6 +33,9 @@ but for greater control the following urls are used.
 
 urlpatterns = [
 
+    # include bookmark app urls
+    url(r'^home/', include('bookmark_app.urls')),
+
     # registration with email confirmation
     url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^account_activation_sent/$', core_views.account_activation_sent, 
@@ -41,8 +44,7 @@ urlpatterns = [
         core_views.activate, name='activate'),
 
     # auth
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 
     # password reset
