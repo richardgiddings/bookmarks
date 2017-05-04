afdd0295c0bbf997bdd2c6bae3d262640432bbea
Richard Giddings <mail@richardgiddings.co.uk>
2017-05-04 13:52:56 +0100

    Static file changs to settings

--- django_login/settings/production.py
+++ django_login/settings/production.py
@@ -23,7 +23,7 @@ DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
