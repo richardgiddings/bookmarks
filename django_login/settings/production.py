f1cb2518278aea4c3a52c67a246c1b8c3452d687
Richard Giddings <mail@richardgiddings.co.uk>
2017-05-05 12:42:36 +0100

    Turn off whitenoise gzip

--- django_login/settings/production.py
+++ django_login/settings/production.py
@@ -32,7 +32,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # email settings
 EMAIL_HOST = env['EMAIL_HOST']
